537927f726b904215c05860345520ecb0fdaaf95
Brian Floersch <gh123man@gmail.com>
2015-02-10 14:52:09 -0500

    ui improvements

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
+<html>
+
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <style>
+        .hightlight {
+            background: rgba(247, 250, 80, 0.64);
+        }
+        .hover:hover {
+            background: #F0F0F0;
+        }
+        tr, td {
+            padding: 5px;
+            border: 0px;
+        }
+        body {
+            font-family: 'Open Sans', sans-serif;
+        }
+        table {border: none;}
+    </style>
+</head>
+
+<body>
 <?php
 include_once './creds.php';
 include_once './CurlFunctions.php';
 
 session_start();
-
 
 if (!isset($_SESSION['clientSession'])) {
     $_SESSION['clientSession'] = CurlFunctions::login(HOST, USERNAME, PASSWORD);
@@ -29,13 +51,19 @@ printNonStatic($nonStaticData);
 
 function printNonStatic($data) {
     ?>
-    <h3>Non-Static Mappings</h3>
-    <table>
+    <h3>DHCP Addresses</h3>
+    <table cellspacing="0" cellpadding="0">
+        <tr>
+            <th></th>
+            <th>Computer Name</th>
+            <th>IP</th>
+        </tr>
         <?php
 
         foreach ($data as $ip => $data) {
             $name = $data->{'client-hostname'};
-            echo '<tr>';
+
+            drawYouCheck($ip);
 
             echo '<td>';
             echo $name;
@@ -57,12 +85,18 @@ function printStatic($data) {
     ?>
 
     <h3>Static Mappings</h3>
-    <table>
+    <table cellspacing="0" cellpadding="0">
+        <tr>
+            <th></th>
+            <th>Computer Name</th>
+            <th>IP</th>
+        </tr>
         <?php
 
         foreach ($data as $name => $data) {
             $ip = $data->{'ip-address'};
-            echo '<tr>';
+
+            drawYouCheck($ip);
 
             echo '<td>';
             echo $name;
@@ -79,6 +113,15 @@ function printStatic($data) {
     <?php
 }
 
+function drawYouCheck($ip) {
+    if ($ip == $_SERVER['REMOTE_ADDR']) {
+        echo '<tr class="hightlight hover">';
+        echo '<td>YOU</td>';
+    } else {
+        echo '<tr class="hover">';
+        echo '<td></td>';
+    }
+}
 
 function getDhcp($session) {
     $data = json_decode(CurlFunctions::get(HOST . 'api/edge/data.json?data=dhcp_leases', $session));
@@ -108,3 +151,5 @@ function getNonStatic($dhcp) {
 
 
 ?>
+</body>
+</html>
